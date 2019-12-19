f2583a5dd13c0c191eceb7a87a86a35c3263e978
IrNesterova <32297223+IrNesterova@users.noreply.github.com>
2019-12-19 18:05:48 +0300

    5thhomework?

---
+++ TestHW/src/ru/itis/JsonTestGenerator.java
@@ -0,0 +1,15 @@
+package ru.itis;
+
+import org.codehaus.jackson.JsonEncoding;
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonGenerator;
+import org.json.simple.JSONObject;
+import org.openqa.selenium.json.JsonOutput;
+
+import java.io.File;
+
+public class JsonTestGenerator {
+    public static void main(String[] args) {
+
+    }
+}
